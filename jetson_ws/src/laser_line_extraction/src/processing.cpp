97ab6665c8624e6505f6cf2137adb38562ae33ab
lalywr2000 <12190450@inha.edu>
2024-08-16 04:47:17 +0900

    go to sleep

--- jetson_ws/src/laser_line_extraction/src/processing.cpp
+++ jetson_ws/src/laser_line_extraction/src/processing.cpp
@@ -2,11 +2,49 @@
 #include "laser_line_extraction/LineSegment.h"
 #include "laser_line_extraction/LineSegmentList.h"
 
+#include <cmath>
 #include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<float>> line_info;
+float x_start, y_start, x_end, y_end;
+
+void getDist(float x1, float y1, float x2, float y2) {
+  return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+}
 
 void msgCallback(const laser_line_extraction::LineSegmentList msg) {
-  std::cout << msg.line_segments.size() << std::endl;
   // ROS_INFO("Subscribed!");
+
+  line_info.clear();
+
+  if (msg.line_segments.size() > 0) {
+    for (int i = 0; i < msg.line_segments.size(); i++) {
+      x_start = msg.line_segments[i].start[0];
+      y_start = msg.line_segments[i].start[1];
+      x_end = msg.line_segments[i].end[0];
+      y_end = msg.line_segments[i].end[1];
+
+      line_info.push_back({getDist(x_start, y_start, x_end, y_end), x_start, y_start, x_end, y_end});
+    }
+
+    sort(line_info.begin(), line_info.end(), [](const vector<float>& a, const vector<float>& b) { return a[0] < b[0]; });
+
+    for (int i = 1; i < 5; i++) {
+      cout << line_info[0][i] << endl;
+      if (i == 2) {
+        cout << "---" << endl;
+      }
+    }
+    cout << "===============" << endl;
+  }
+
+
+
+  // pub.publish(msg);
+  // ROS_INFO("Published!");
 }
 
 int main(int argc, char **argv) {
@@ -14,6 +52,7 @@ int main(int argc, char **argv) {
   ros::NodeHandle n;
 
   ros::Subscriber sub = n.subscribe("/line_segments", 1, msgCallback);
+  // ros::Publisher pub = n.advertise<std_msgs::String>("std_msg", 1000);
 
   ros::spin();
 
@@ -22,25 +61,9 @@ int main(int argc, char **argv) {
 
 
 
-// ============
-// int main(int argc, char **argv)
-// {
-//   ros::init(argc, argv, "pub_node");
-//   ros::NodeHandle n;
-//   ros::Publisher pub = n.advertise<std_msgs::String>("std_msg", 1000);
-//   ros::Rate loop_rate(10);
-
-//   while (ros::ok())
-//   {
-//     std_msgs::String msg;
-//     msg.data = "hello world";
-
-//     pub.publish(msg);
-//     ROS_INFO("Published!");
-
-//     ros::spinOnce();
-//     loop_rate.sleep();
-//   }
-
-//   return 0;
-// }
+/*
+1. get nearest wall and distance of it.
+2. select appropriate distance and make control code to keep the distance.
+3. consider the angle of the wall and make it align to the wall.
+4. examine the 4 direction of the wall and follow the wall CCW.
+*/
