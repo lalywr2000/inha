a9c52f4dfe7a0329d91ec446e06febcbfb56549a
lalywr2000 <12190450@inha.edu>
2024-08-18 00:50:09 +0900

    a

--- jetson_ws/src/laser_line_extraction/src/processing.cpp
+++ jetson_ws/src/laser_line_extraction/src/processing.cpp
@@ -11,7 +11,7 @@ using namespace std;
 vector<vector<float>> line_info;
 float x_start, y_start, x_end, y_end;
 
-void getDist(float x1, float y1, float x2, float y2) {
+float getDist(float x1, float y1, float x2, float y2) {
   return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
 }
 
