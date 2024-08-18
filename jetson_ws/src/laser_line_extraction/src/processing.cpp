42d6e6c97b86af6518ae3f648d94b3ac713fa41c
lalywr2000 <12190450@inha.edu>
2024-08-18 16:13:16 +0900

    a

--- jetson_ws/src/laser_line_extraction/src/processing.cpp
+++ jetson_ws/src/laser_line_extraction/src/processing.cpp
@@ -21,13 +21,15 @@ namespace Color {
   const vector<float> GRAY    = {0.5f, 0.5f, 0.5f, 1.0f};
 }
 
-ros::Subscriber sub;
-ros::Publisher pub;
+
+ros::Publisher visualize_;
 
 vector<vector<float>> line_info;
-float x_start, y_start, x_end, y_end, dist, len;
-
 visualization_msgs::MarkerArray marker_array;
+
+float x_start, y_start, x_end, y_end, dist, len;
+bool update_1 = false;
+bool update_2 = false;
 
 
 void visualizeLine(vector<float> start_point,
@@ -112,14 +114,16 @@ float getLineDist(float x1, float y1, float x2, float y2) {
   return abs(intercept) / sqrt(incline * incline + 1.0f);
 }
 
-void msgCallback(const laser_line_extraction::LineSegmentList msg) {
-  // ROS_INFO("Subscribed!");
+void visualize() {
+  visualizePoint({0.0f, 0.0f}, Color::ORANGE, 0.2f);  // visualize ego vehilcle here !!!
+
+  visualize.publish(marker_array);
 
   line_info.clear();
   marker_array.markers.clear();
+}
 
-  visualizePoint({0.0f, 0.0f}, Color::ORANGE, 0.2f);
-
+void topic_callback_1(const laser_line_extraction::LineSegmentList msg) {
   if (msg.line_segments.size() > 0) {
     for (int i = 0; i < msg.line_segments.size(); i++) {
       x_start = msg.line_segments[i].start[0];
@@ -148,28 +152,75 @@ void msgCallback(const laser_line_extraction::LineSegmentList msg) {
     }
   }
 
-  pub.publish(marker_array);
-  // ROS_INFO("Published!");
+  update_1 = true;
+
+  if (update_1 && update_2) {
+    visualize();
+
+    update_1 = false;
+    update_2 = false;
+  }
 }
 
+void topic_callback_2(const laser_line_extraction::LineSegmentList msg) {
+  if (msg.line_segments.size() > 0) {
+    for (int i = 0; i < msg.line_segments.size(); i++) {
+      x_start = msg.line_segments[i].start[0];
+      y_start = msg.line_segments[i].start[1];
+      x_end = msg.line_segments[i].end[0];
+      y_end = msg.line_segments[i].end[1];
+
+      if (isnan(x_start) || isnan(y_start) || isnan(x_end) || isnan(y_end)) {
+        continue;
+      }
+
+      dist = getLineDist(x_start, y_start, x_end, y_end);
+      len = getDist(x_start, y_start, x_end, y_end);
+
+      if (dist < 5.0f && len > 1.0f) {
+        line_info.push_back({dist, x_start, y_start, x_end, y_end});
+      }
+    }
+
+    if (line_info.size() > 0) {
+      sort(line_info.begin(), line_info.end(), [](const vector<float>& a, const vector<float>& b) { return a[0] < b[0]; });
+
+      for (int i = 0; i < line_info.size(); i++) {
+        visualizeLine({line_info[i][1], line_info[i][2]}, {line_info[i][3], line_info[i][4]}, Color::WHITE, 0.1f);
+      }
+    }
+  }
+
+  update_2 = true;
+
+  if (update_1 && update_2) {
+    visualize();
+
+    update_1 = false;
+    update_2 = false;
+  }
+}
 
 int main(int argc, char **argv) {
+  line_info.clear();
+  marker_array.markers.clear();
+  
   ros::init(argc, argv, "processing_node");
   ros::NodeHandle n;
 
-  sub = n.subscribe("/line_segments", 1, msgCallback);
-  pub = n.advertise<visualization_msgs::MarkerArray>("/vis", 1);
+  ros::Subscriber subscription_1 = n.subscribe("/line_segments_1", 1, topic_callback_1);
+  ros::Subscriber subscription_2 = n.subscribe("/line_segments_2", 1, topic_callback_2);
+  visualize_ = n.advertise<visualization_msgs::MarkerArray>("/vis", 1);
 
   ros::spin();
 
   return 0;
 }
 
+
 /*
-+ draw base robot, wheels and position of lidar
 1. get nearest wall and distance of it.
 2. select appropriate distance and make control code to keep the distance.
 3. consider the angle of the wall and make it align to the wall.
 4. examine the 4 direction of the wall and follow the wall CCW.
-+ AEB
 */
