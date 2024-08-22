eca2424820ceda4c5ccf0bd6d526944e550e7910
lalywr2000 <12190450@inha.edu>
2024-08-22 22:42:39 +0900

    python node

---
+++ jetson_ws/src/control/src/contol.py
@@ -0,0 +1,56 @@
+import rospy
+from laser_line_extraction import MainWall
+
+
+WALL_DRIVE_DIST = 1.1
+WALL_DROP_DIST  = 0.8
+
+WALL_DIST_NOISE_BAND = 0.2
+INCLINE_ALIGN_NOISE_BAND = 0.3
+
+mission_num = 1
+
+
+def mission_1(distance, incline):
+	global mission_num
+	if (mission_num == 1):
+		print(distance, incline)
+  
+	return
+
+
+def mission_2(distance, incline):
+	global mission_num
+	if (mission_num == 2):
+		pass
+	
+	return
+
+
+def mission(distance, incline):
+	mission_1(distance, incline)
+	mission_2(distance, incline)
+
+	return
+
+
+def msgCallback(msg):
+	mission(msg.distance, msg.incline)
+
+	return
+
+
+def sub():
+	rospy.init_node('control_node', anonymous=True)
+	rospy.Subscriber("/main_wall", MainWall, msgCallback)
+
+	rospy.spin()
+
+
+if __name__ == '__main__':
+	sub()
+
+
+# 2. control to keep the distance. go find wall to certain direction and make it stop.
+# 3. consider the angle of the wall and make it align to the wall.
+# 4. examine the 4 direction of the wall and follow the wall CCW.
