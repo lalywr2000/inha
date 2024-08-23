a4107ddec2490ac0350e64007ae7a8fd6c33af6e
lalywr2000 <12190450@inha.edu>
2024-08-23 16:06:28 +0900

    a

--- jetson_ws/src/control/src/control.py
+++ jetson_ws/src/control/src/control.py
@@ -1,6 +1,8 @@
 import rospy
 from geometry_msgs import Point
 
+from mecanumwheel import SerialComm
+
 
 WALL_DRIVE_DIST = 1.1
 WALL_DROP_DIST  = 0.8
@@ -8,25 +10,57 @@ WALL_DROP_DIST  = 0.8
 WALL_DIST_NOISE_BAND = 0.2
 INCLINE_ALIGN_NOISE_BAND = 0.3
 
+FRONT_WALL_PARALLEL_CONDITION = 0.1
+
+DRIVE_SPEED = 3.0
+
+
+robot = SerialComm(port='/dev/ttyACM0', baudrate=9600, timeout=1)
+
 mission_num = 1
 
 
-def mission_1(distance, incline):  # Move Forward
+# speed [km/h], angle [deg], rotation [-1.0: CW, 0.0: N/A, 1.0: CCW]
+
+
+def mission_1(distance, incline):  # move forward to the wall
 	global mission_num
 	if (mission_num == 1):
 
 		if distance > WALL_DRIVE_DIST:
-			# move forard
+			speed = DRIVE_SPEED
+			angle = 90.0
+			if incline > FRONT_WALL_PARALLEL_CONDITION:
+				rotation = 1.0
+			elif incline < -1.0 * FRONT_WALL_PARALLEL_CONDITION:
+				rotation = -1.0
+			else:
+				rotation = 0.0
+            
+			robot.move_data(speed, angle, rotation)
+		
 		else:
-			# stop signal
+			speed = 0.0
+			angle = 90.0
+			rotation = 0.0
+            
+			robot.move_data(speed, angle, rotation)
 			mission_num += 1
 
 	return
 
 
-# def mission_2(distance, incline):
+# def mission_2(distance, incline):  # Turn Left
 # 	global mission_num
 # 	if (mission_num == 2):
+# 		pass
+	
+# 	return
+
+
+# def mission_3(distance, incline):  # Move Forward
+# 	global mission_num
+# 	if (mission_num == 3):
 # 		pass
 	
 # 	return
@@ -35,6 +69,7 @@ def mission_1(distance, incline):  # Move Forward
 def msgCallback(msg):
 	mission_1(msg.x, msg.y)
 	# mission_2(msg.x, msg.y)
+    # mission_3(msg.x, msg.y)
 
 	return
 
@@ -43,13 +78,14 @@ def sub():
 	rospy.init_node('control_node', anonymous=True)
 	rospy.Subscriber("/main_wall", Point, msgCallback)
 
-	rospy.spin()
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		pass
+	finally:
+		robot.kill()
+		robot.close()
 
 
 if __name__ == '__main__':
 	sub()
-
-
-# 2. control to keep the distance. go find wall to certain direction and make it stop.
-# 3. consider the angle of the wall and make it align to the wall.
-# 4. examine the 4 direction of the wall and follow the wall CCW.
