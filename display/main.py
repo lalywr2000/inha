eaf4838a516e0a77f9a56091334a88ffc6fe3b45
lalywr2000 <12190450@inha.edu>
2024-11-16 15:44:40 +0900

    a

--- display/main.py
+++ display/main.py
@@ -2,13 +2,16 @@ import os
 import sys
 import time
 import pygame
+from datetime import datetime
+import pytz
 
 dir = os.getcwd()
 
 pygame.init()
 pygame.display.set_caption("inha application")
 screen = pygame.display.set_mode((1280, 720), pygame.FULLSCREEN)
-text = pygame.font.SysFont("bold", 75)
+small_text = pygame.font.SysFont("Arial", 50)
+big_text = pygame.font.SysFont("Arial", 130)
 
 
 display_img = pygame.image.load(dir + r'/object/display.png').convert_alpha()
@@ -27,11 +30,23 @@ class Page:
 
         screen.blit(display_img, (0, 0))
 
-        # screen.blit(text.render("asdfasdf", True, (0, 0, 0)), (97, 263))
+        korea_timezone = pytz.timezone('Asia/Seoul')
+        korea_time = datetime.now(korea_timezone)
+        year = korea_time.year
+        month = korea_time.month
+        day = korea_time.day
+        hour = korea_time.hour
+        minute = korea_time.minute
+        second = korea_time.second
 
-        # screen.fill((255, 255, 255))
-        # pygame.draw.rect(screen, (0, 0, 0), (0, 0, 20, 20))
-        # pygame.draw.rect(screen, (0, 0, 0), (1260, 700, 20, 20))
+        screen.blit(pygame.transform.rotate(small_text.render('{}. {}. {}.'.format(year, month, day),
+        True, (255, 255, 255)), 90), (35, 370))
+        if second % 2 == 0:
+            screen.blit(pygame.transform.rotate(big_text.render(str(hour).zfill(2) + ':' + str(minute).zfill(2),
+            True, (255, 255, 255)), 90), (100, 350))
+        else:
+            screen.blit(pygame.transform.rotate(big_text.render(str(hour).zfill(2) + ' ' + str(minute).zfill(2),
+            True, (255, 255, 255)), 90), (100, 350))
 
         return
 
