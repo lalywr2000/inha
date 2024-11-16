bdcf280ea1b1e8ca638ec309daa67b1c8d5ad935
lalywr2000 <12190450@inha.edu>
2024-11-16 14:46:52 +0900

    a

--- display/main.py
+++ display/main.py
@@ -13,9 +13,9 @@ text = pygame.font.SysFont("bold", 75)
 
 display_img = pygame.image.load(dir + r'/object/display.png').convert_alpha()
 
-# scale_factor = 0.46
+scale_factor = 0.46
 
-# background_img = pygame.transform.scale(background_img, (int(background_img.get_size()[0] * scale_factor), int(background_img.get_size()[1] * scale_factor)))
+display_img = pygame.transform.scale(display_img, (int(display_img.get_size()[0] * scale_factor), int(display_img.get_size()[1] * scale_factor)))
     
 
 class Page:
