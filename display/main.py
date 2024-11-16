33a1a40dc6aa2f6731c261fd268edf6d5809f2a0
lalywr2000 <12190450@inha.edu>
2024-11-16 11:37:29 +0900

    makerthon

---
+++ display/main.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import time
+import pygame
+
+dir = os.getcwd()
+
+pygame.init()
+pygame.display.set_caption("inha application")
+screen = pygame.display.set_mode((1280, 720), pygame.FULLSCREEN)
+text = pygame.font.SysFont("bold", 75)
+
+
+background_img = pygame.image.load(dir + r'/object/background.png').convert_alpha()
+
+scale_factor = 0.46
+
+background_img = pygame.transform.scale(background_img, (int(background_img.get_size()[0] * scale_factor), int(background_img.get_size()[1] * scale_factor)))
+    
+
+class Page:
+    def draw(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.blit(background_img, (25, 0))
+
+        screen.blit(text.render("asdfasdf", True, (0, 0, 0)), (97, 263))
+
+        return
+
+
+page = Page()
+
+while True:
+    page.draw()
+    pygame.display.update()
