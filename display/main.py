65efb84f568867ac51439763d01024e6c181f7ad
lalywr2000 <12190450@inha.edu>
2024-11-16 16:43:09 +0900

    a

--- display/main.py
+++ display/main.py
@@ -15,14 +15,23 @@ big_text = pygame.font.SysFont("Arial", 140)
 
 
 display_img = pygame.image.load(dir + r'/object/display.png').convert_alpha()
+warn1_img = pygame.image.load(dir + r'/object/warn1.png').convert_alpha()
+warn2_img = pygame.image.load(dir + r'/object/warn2.png').convert_alpha()
 
 scale_factor = 0.435
 
 display_img = pygame.transform.scale(display_img, (int(display_img.get_size()[0] * scale_factor), int(display_img.get_size()[1] * scale_factor)))
-    
+warn1_img = pygame.transform.scale(warn1_img, (int(warn1_img.get_size()[0] * scale_factor), int(warn1_img.get_size()[1] * scale_factor)))
+warn2_img = pygame.transform.scale(warn2_img, (int(warn2_img.get_size()[0] * scale_factor), int(warn2_img.get_size()[1] * scale_factor)))
+
+warning = True
+warn_screen = False
+
 
 class Page:
     def draw(self):
+        global warning, warn_screen
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -48,6 +57,15 @@ class Page:
             screen.blit(pygame.transform.rotate(big_text.render(str(hour).zfill(2) + ' ' + str(minute).zfill(2),
             True, (255, 255, 255)), 90), (100, 330))
 
+        if warning:
+            if warn_screen:
+                screen.blit(warn1_img, (0, -56))
+            else:
+                screen.blit(warn2_img, (0, -56))
+
+            time.sleep(0.5)
+            warn_screen = not warn_screen
+
         return
 
 
@@ -56,3 +74,9 @@ page = Page()
 while True:
     page.draw()
     pygame.display.update()
+
+
+
+
+
+
